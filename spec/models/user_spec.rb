7e2c4d92ddd21a327ead2a0996fc38a707fa83d7
Torgny Bjers <torgny@xorcode.com>
2015-09-12 15:22:23 -0400

    Cleaned up configs and added specs for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+  it 'has encrypted_password' do
+    expect(FactoryGirl.create(:user).encrypted_password).not_to be_empty
+  end
+  it 'does not validate with a short password' do
+    expect(FactoryGirl.build(:user, password: Faker::Internet.password(4))).not_to be_valid
+  end
+  it 'does not validate with a too long password' do
+    expect(FactoryGirl.build(:user, password: Faker::Internet.password(90))).not_to be_valid
+  end
+  it 'does not allow duplicate emails' do
+    email = Faker::Internet.email
+    expect(FactoryGirl.create(:user, email: email)).to be_valid
+    expect(FactoryGirl.build(:user, email: email)).not_to be_valid
+  end
 end
